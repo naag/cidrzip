8d55db5129b169a29f0115968a475a50e49cc4b1
Peter Bücker <peter.buecker@gmail.com>
2025-02-12 22:40:51 +0100

    feat: specify MIT license

--- cidr_grouping.py
+++ cidr_grouping.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Copyright (c) 2025 Peter Bücker
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 import ipaddress
 from typing import List
